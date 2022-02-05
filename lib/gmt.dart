eab7cf3cddf935a2db6aa58ca615af7a326c4163
Fatasin <contact.fatasin@gmail.com>
2022-02-05 18:33:17 +0700

    Initial commit

---
+++ lib/gmt.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+class GMT {
+  /// Get current datetime from internet (example.com or google.com) and
+  /// return DateTime in UTC.
+  ///
+  /// [returnLocalIfError] == true: Return local [DateTime.now()] if error or timeout.
+  /// [returnLocalIfError] == false: Return [null] if error or timeout.
+  static Future<DateTime?> now(
+      {bool returnLocalIfError = true, Duration? timeout}) async {
+    DateTime? now;
+    if (timeout != null) {
+      now = await _now().timeout(timeout, onTimeout: () => null);
+    } else {
+      now = await _now();
+    }
+    if (now == null && returnLocalIfError) {
+      return DateTime.now();
+    }
+    return now;
+  }
+
+  static Future<DateTime?> _now() async {
+    var response = await http.get(Uri.parse('https://www.example.com'));
+    if (response.statusCode != 200) {
+      response = await http.get(Uri.parse('https://www.google.com'));
+    }
+    String? dateTime = response.headers['date'];
+    if (response.statusCode == 200 && dateTime != null) {
+      return HttpDate.parse(dateTime).toUtc();
+    } else {
+      return null;
+    }
+  }
+}
---
+++ test/gmt_test.dart
@@ -0,0 +1,8 @@
+import 'package:gmt/gmt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('getPlatformVersion', () async {
+    print(await GMT.now());
+  });
+}
