65af3ab56ea78128ea4fa88d574cb9ba1c577378
Fatasin <contact.fatasin@gmail.com>
2022-02-27 14:48:06 +0700

    Supported web platform

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.0
+
+* Supported web platform.
+
 ## 0.0.2
 
 * Bug fixed: Convert local `DateTime.now()` to UTC.
--- lib/gmt.dart
+++ lib/gmt.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'package:universal_io/io.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 
